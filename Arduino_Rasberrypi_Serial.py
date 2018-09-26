f778f755d204a9c4a9bb6c8b51f0fafa459c736a
Jinu <39368262+ruldy99@users.noreply.github.com>
2018-09-26 21:26:50 +0900

    서버와의 완전한 통신
    최종코드

--- Arduino_Rasberrypi_Serial.py
+++ Arduino_Rasberrypi_Serial.py
@@ -3,49 +3,52 @@
 #서버로 전달하고 서버에서 보내는 값을 받는다.
 
 
+//서버와의 코드
 
-
-import serial
-import urllib request
-import RPi GPIO as GPIO
+import serial 
+import urllib.request
+import RPi.GPIO as GPIO
 import time
 
-GPIO setmode(GPIO.BCM)
-port="/dev/ttyACM0"
+GPIO.setmode(GPIO.BCM) 
+port = "/dev/ttyACM0" 
 
-ser=serial.Serial(port,9600)
-ser.flushInput()
+ser = serial.Serial(port, 9600) 
+ser.flushInput() 
 status='safe';
 count=0;
 
 try:
     GPIO.setmode(GPIO.BCM)
-    input=0
+    input =0
     while True:
         sign = ser.readline()
-        data=sign.decode('utf-8').split('\\')[0]
-        if data==1:
-            status='warning'
-            data_b={'status':staus}
-            data_par=urllib.parse.urlencode(data_b).encode("utf-8")
-            req=urllib.request.Request(url='http://ipAddress/name',data=data_par.method='PUT')
-            response=urllib.request.urlopen(req)
-            'Read from Server' = response.read()
-             
+        data = sign.decode('utf-8').split('\\')[0]
+        print(data)
+        if data == '1':
+                                          status='warning'
+                                          data_b = {'question_text':status}
+                                          data_par = urllib.parse.urlencode(data_b).encode("utf-8")
+
+                                          req=urllib.request.Request(url='http://222.117.181.227:8080/polls/save', data= data_par,method='PUT')
+                                          
+                                          response = urllib.request.urlopen(req)
+                                          page = response.read()
+                                          print(page)
 
         
+
         else:
-            status='safe'
-            data_b={'status':status}
-            data_par=urllib.parse.urlencode(data_b).encode("utf-8")
-            req=urllib.request.Request(url='http://ipAdress/name',data=data_par.method='PUT')
-            response=urlib.request.urlopen(req)
-            'Read from SErver'=response.read()
-             
+                                          status='safe'
+                                          data_b = {'question_text':status}
+                                          data_par = urllib.parse.urlencode(data_b).encode("utf-8")
+
+                                          req=urllib.request.Request(url='http://222.117.181.227:8080/polls/save', data= data_par,method='PUT')
+                                          
+##                                          url='http://220.67.124.128:8000/yun/test'
+                                          response = urllib.request.urlopen(req)
+                                          page = response.read()
+                                          print(page)
+                                                
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-
-
-
-
